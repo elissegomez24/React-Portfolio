434848f495131c2d8efdd8f077b98097ce992d06
elissegomez24 <elissegomez24@gmail.com>
2025-01-16 14:30:27 -0500

    Updated About and Portolio Section

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -11,6 +11,9 @@ export default function Portfolio() {
                     <div className="portfolio-layer">
                         <div className="portfolio-title">
                             <h4>Employee Payroll Tracker</h4>
+                            <p className="portfolio-description">
+                            The application allows users to view, add, and update information about departments, roles, and employees, to organize and plan business operations.
+                            </p>
                             <div className="portfolio-links">
                                 <a href="https://elissegomez24.github.io/JavaScript-Challenge/" aria-label="View Employee Payroll Tracker live site">
                                     <i className="bx bx-link-external"></i>
@@ -29,6 +32,9 @@ export default function Portfolio() {
                     <div className="portfolio-layer">
                         <div className="portfolio-title">
                             <h4>My Personal Blog</h4>
+                            <p className="portfolio-description">
+                                This is a personal blog application that allows users to submit blog posts with a username, title, and content.
+                            </p>
                             <div className="portfolio-links">
                                 <a href="https://elissegomez24.github.io/Elisse-Blog/" aria-label="View My Personal Blog live site">
                                     <i className="bx bx-link-external"></i>
@@ -47,6 +53,9 @@ export default function Portfolio() {
                     <div className="portfolio-layer">
                         <div className="portfolio-title">
                             <h4>Task Board</h4>
+                            <p className="portfolio-description">
+                                This project implements a Kanban board for managing tasks. It allows users to add new tasks with titles, descriptions, and deadlines. 
+                            </p>
                             <div className="portfolio-links">
                                 <a href="https://elissegomez24.github.io/Task-Board/" aria-label="View Task Board live site">
                                     <i className="bx bx-link-external"></i>
@@ -65,6 +74,9 @@ export default function Portfolio() {
                     <div className="portfolio-layer">
                         <div className="portfolio-title">
                             <h4>Weather Dashboard</h4>
+                            <p className="portfolio-description">
+                                A web application that allows users to search for weather information of cities and view current conditions as well as a 5-day forecast.
+                            </p>
                             <div className="portfolio-links">
                                 <a href="https://elissegomez24.github.io/Weather-Dashboard/" aria-label="View Weather Dashboard live site">
                                     <i className="bx bx-link-external"></i>
@@ -83,6 +95,9 @@ export default function Portfolio() {
                     <div className="portfolio-layer">
                         <div className="portfolio-title">
                             <h4>Book Search Engine</h4>
+                            <p className="portfolio-description">
+                                The Book Search Engine is a web application that allows users to search for books using the Google Books API.
+                            </p>
                             <div className="portfolio-links">
                                 <a href="https://book-search-engine-3qy8.onrender.com/" aria-label="View Book Search Engine live site">
                                     <i className="bx bx-link-external"></i>
@@ -101,6 +116,9 @@ export default function Portfolio() {
                     <div className="portfolio-layer">
                         <div className="portfolio-title">
                             <h4>Redux Store</h4>
+                            <p className="portfolio-description">
+                                The application allows users to browse products, add items to a shopping cart, view details about products, and checkout.
+                            </p>
                             <div className="portfolio-links">
                                 <a href="https://redux-store-g8ui.onrender.com/" aria-label="View Redux Store live site">
                                     <i className="bx bx-link-external"></i>
@@ -116,4 +134,3 @@ export default function Portfolio() {
         </section>
     );
 }
-
